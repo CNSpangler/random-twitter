6256993bafa65b6fc96a1a07869b2dcae1f0e4c3
Chelsea Spangler <chelseanspangler@gmail.com>
2020-03-24 15:51:12 -0700

    test written for Comment post route

---
+++ __tests__/comments.test.js
@@ -0,0 +1,159 @@
+// * `POST /api/v1/comments` create a comment
+// * `GET /api/v1/comments/:id` get a comment by id and populate tweet
+// * `PATCH /api/v1/comments/:id` update a comment
+// * `DELETE /api/v1/comments/:id` delete a comment
+
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Tweet = require('../lib/models/Tweet');
+const Comment = require('../lib/models/Comment');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('creates a comment and posts it to a specific tweet', async() => {
+    const tweet = await Tweet.create(
+      { handle: 'user', text: 'I\'m going to type every word I know.' },
+    );
+
+    return request(app)
+      .send({
+        tweetId: tweet.id,
+        handle: 'user2',
+        text: 'rectangle'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          tweetId: tweet.id,
+          handle: 'user2',
+          text: 'rectangle',
+          __v: 0
+        });
+      });
+  });
+
+
+  // create a tweet
+  //
+
+  // it('creates a new tweet with user text', () => {
+  //   return request(app)
+  //     .post('/api/v1/tweets')
+  //     .send({
+  //       handle: 'someone',
+  //       text: 'People who buy things are suckers.'
+  //     })
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         _id: expect.any(String),
+  //         handle: 'someone',
+  //         text: 'People who buy things are suckers.',
+  //         __v: 0
+  //       });
+  //     });
+  // });
+
+  // it('creates a new tweet with random quote from API', () => {
+  //   return request(app)
+  //     .post('/api/v1/tweets')
+  //     .send({
+  //       handle: 'someone',
+  //       text: ''
+  //     })
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         _id: expect.any(String),
+  //         handle: 'someone',
+  //         text: expect.any(String),
+  //         __v: 0
+  //       });
+  //     });
+  // });
+
+  // it('gets all tweets', async() => {
+  //   const tweets = await Tweet.create([
+  //     { handle: 'user', text: 'rectangle' },
+  //     { handle: 'user', text: 'America' },
+  //     { handle: 'user', text: 'butthole' },
+  //   ]);
+
+  //   return request(app)
+  //     .get('/api/v1/tweets')
+  //     .then(res => {
+  //       tweets.forEach(tweet => {
+  //         expect(res.body).toContainEqual({
+  //           _id: tweet._id.toString(),
+  //           handle: tweet.handle,
+  //           text: tweet.text,
+  //           __v: 0
+  //         });
+  //       });
+  //     });
+  // });
+
+  // it('gets a tweet by id', async() => {
+  //   const tweet = await Tweet.create(
+  //     { handle: 'user', text: 'rectangle' },
+  //   );
+
+  //   return request(app)
+  //     .get(`/api/v1/tweets/${tweet._id}`)
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         _id: expect.any(String),
+  //         handle: 'user', 
+  //         text: 'rectangle',
+  //         __v: 0
+  //       });
+  //     });
+  // });
+
+  // it('updates a tweet by id', async() => {
+  //   const tweet = await Tweet.create(
+  //     { handle: 'user', text: 'rectangle' },
+  //   );
+
+  //   return request(app)
+  //     .patch(`/api/v1/tweets/${tweet._id}`)
+  //     .send({ text: 'America' })
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         _id: expect.any(String),
+  //         handle: 'user',
+  //         text: 'America',
+  //         __v: 0
+  //       });
+  //     });
+  // });
+
+  // it('deletes a tweet by id', async() => {
+  //   const tweet = await Tweet.create(
+  //     { handle: 'user', text: 'rectangle' },
+  //   );
+  //   return request(app)
+  //     .delete(`/api/v1/tweets/${tweet._id}`)
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         _id: expect.any(String),
+  //         handle: 'user', 
+  //         text: 'rectangle',
+  //         __v: 0
+  //       });
+  //     });
+  // });
+});
