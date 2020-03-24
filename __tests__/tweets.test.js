9335fcc0cf2fc9cb7fde653291d0ac6ab425d890
Chelsea Spangler <chelseanspangler@gmail.com>
2020-03-24 15:06:31 -0700

    failing test for get all route

--- __tests__/tweets.test.js
+++ __tests__/tweets.test.js
@@ -5,7 +5,7 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Tweet = require('../lib/models/Tweet');
-const getQuotes = require('../lib/services/quotes');
+// const getQuotes = require('../lib/services/quotes');
 
 // jest.mock('../lib/services/lyrics.js', () => {
 //   return () => Promise.resolve('some fake lyrics');
@@ -29,18 +29,60 @@ describe('app routes', () => {
     return mongoose.connection.close();
   });
 
-  it('gets a quote and creates a new tweet', () => {
+  it('creates a new tweet with user text', () => {
     return request(app)
-      .get('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
-      .send([expect.any(String)])
+      .post('/api/v1/tweets')
+      .send({
+        handle: 'someone',
+        text: 'People who buy things are suckers.'
+      })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          quote: expect.any(String),
+          handle: 'someone',
+          text: 'People who buy things are suckers.',
           __v: 0
         });
       });
   });
+
+  it('creates a new tweet with random quote from API', () => {
+    return request(app)
+      .post('/api/v1/tweets')
+      .send({
+        handle: 'someone',
+        text: ''
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          handle: 'someone',
+          text: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+
+  it('gets all tweets', async() => {
+    const tweets = await Tweet.create([
+      { handle: 'user', text: 'rectangle' },
+      { handle: 'user', text: 'America' },
+      { handle: 'user', text: 'butthole' },
+    ]);
+
+    return request(app)
+      .get('/api/v1/tweets')
+      .then(res => {
+        tweets.forEach(tweet => {
+          expect(res.body).toContainEqual({
+            _id: tweet._id.toString(),
+            handle: tweet.handle,
+            text: tweet.text
+          });
+        });
+      });
+  });
+
 
 //   it('gets all playlist seeds', () => {
 //     return PlaylistSeed.create({
