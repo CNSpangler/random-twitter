8833123ed9b1ae9937bc8dea4824b6d92dd68b07
Chelsea Spangler <chelseanspangler@gmail.com>
2020-03-24 16:31:28 -0700

    test written for comment delete route

--- __tests__/comments.test.js
+++ __tests__/comments.test.js
@@ -1,6 +1,3 @@
-// * `PATCH /api/v1/comments/:id` update a comment
-// * `DELETE /api/v1/comments/:id` delete a comment
-
 require('dotenv').config();
 
 const request = require('supertest');
@@ -74,7 +71,6 @@ describe('app routes', () => {
       });
   });
 
-  // * `PATCH /api/v1/comments/:id` update a comment
   it('updates a comment by id', async() => {
     const tweet = await Tweet.create(
       { handle: 'user', text: 'I\'m going to type every word I know.' },
@@ -85,7 +81,7 @@ describe('app routes', () => {
     );
 
     return request(app)
-      .patch('/api/v1/comments/:id')
+      .patch(`/api/v1/comments/${comment._id}`)
       .send({ text: 'America.' })
       .then(res => {
         expect(res.body).toEqual({
@@ -98,99 +94,26 @@ describe('app routes', () => {
       });
   });
   
+  // * `DELETE /api/v1/comments/:id` delete a comment
+  it('deletes a comment by id', async() => {
+    const tweet = await Tweet.create(
+      { handle: 'user', text: 'I\'m going to type every word I know.' },
+    );
 
+    const comment = await Comment.create(
+      { tweetId: tweet.id, handle: 'user2', text: 'Rectangle.' }
+    );
 
-
-
-  // it('updates a tweet by id', async() => {
-  //   const tweet = await Tweet.create(
-  //     { handle: 'user', text: 'rectangle' },
-  //   );
-
-  //   return request(app)
-  //     .patch(`/api/v1/tweets/${tweet._id}`)
-  //     .send({ text: 'America' })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         _id: expect.any(String),
-  //         handle: 'user',
-  //         text: 'America',
-  //         __v: 0
-  //       });
-  //     });
-  // });
-
-
-
-  // it('creates a new tweet with random quote from API', () => {
-  //   return request(app)
-  //     .post('/api/v1/tweets')
-  //     .send({
-  //       handle: 'someone',
-  //       text: ''
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         _id: expect.any(String),
-  //         handle: 'someone',
-  //         text: expect.any(String),
-  //         __v: 0
-  //       });
-  //     });
-  // });
-
-  // it('gets all tweets', async() => {
-  //   const tweets = await Tweet.create([
-  //     { handle: 'user', text: 'rectangle' },
-  //     { handle: 'user', text: 'America' },
-  //     { handle: 'user', text: 'butthole' },
-  //   ]);
-
-  //   return request(app)
-  //     .get('/api/v1/tweets')
-  //     .then(res => {
-  //       tweets.forEach(tweet => {
-  //         expect(res.body).toContainEqual({
-  //           _id: tweet._id.toString(),
-  //           handle: tweet.handle,
-  //           text: tweet.text,
-  //           __v: 0
-  //         });
-  //       });
-  //     });
-  // });
-
-  // it('gets a tweet by id', async() => {
-  //   const tweet = await Tweet.create(
-  //     { handle: 'user', text: 'rectangle' },
-  //   );
-
-  //   return request(app)
-  //     .get(`/api/v1/tweets/${tweet._id}`)
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         _id: expect.any(String),
-  //         handle: 'user', 
-  //         text: 'rectangle',
-  //         __v: 0
-  //       });
-  //     });
-  // });
-
-
-  // it('deletes a tweet by id', async() => {
-  //   const tweet = await Tweet.create(
-  //     { handle: 'user', text: 'rectangle' },
-  //   );
-  //   return request(app)
-  //     .delete(`/api/v1/tweets/${tweet._id}`)
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         _id: expect.any(String),
-  //         handle: 'user', 
-  //         text: 'rectangle',
-  //         __v: 0
-  //       });
-  //     });
-  // });
+    reture request(app)
+      .delete(`/api/v1/comments/${comment._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+            tweetId: tweet.id,
+            _id: expect.any(String), 
+            handle: 'user2', 
+            text: 'Rectangle.',
+            __v: 0
+        })
+      })
+  })
 });
